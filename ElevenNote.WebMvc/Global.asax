0b371b2fad4a5630ca2989dde28cd8f257da6c6a
Justin Foerster <jrfoerster@gmail.com>
2021-10-19 18:23:44 -0400

    Add project files.

---
+++ ElevenNote.WebMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevenNote.WebMvc.MvcApplication" Language="C#" %>
---
+++ ElevenNote.WebMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote.WebMvc.Startup))]
+namespace ElevenNote.WebMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
