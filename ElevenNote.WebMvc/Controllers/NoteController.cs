248df0524d6252f82a0779de499d18d2e1ca18a1
Justin Foerster <jrfoerster@gmail.com>
2021-10-19 19:22:46 -0400

    note index and create views

---
+++ ElevenNote.WebMvc/Controllers/NoteController.cs
@@ -0,0 +1,36 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMvc.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var notes = new NoteListItem[0];
+            return View(notes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
