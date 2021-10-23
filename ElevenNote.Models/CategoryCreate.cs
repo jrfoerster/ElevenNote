3d0f93b5472b6a6d7605186e0c17e02975a4df58
Justin Foerster <jrfoerster@gmail.com>
2021-10-23 11:17:59 -0400

    create new pages, back to list button

--- ElevenNote.Models/CategoryCreate.cs
+++ ElevenNote.Models/CategoryCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace ElevenNote.Models
 {
     public class CategoryCreate
     {
+        [Display(Name = "Category Name")]
         public string CategoryName { get; set; }
     }
 }
